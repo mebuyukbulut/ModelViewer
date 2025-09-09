5889049445bfd68eb6c69d7df2d060a9d2dfcbf7
mebuyukbulut <esatbuyukbulut@gmail.com>
2025-09-10 01:13:41 +0300

    gradient editor added

--- ModelViewer/ColorProvider.cpp
+++ ModelViewer/ColorProvider.cpp
@@ -1,10 +1,123 @@
 #include "ColorProvider.h"
 #include <imgui.h>
+#include <string>
+#include <iostream>
 
+
+// https://github.com/epezent/implot
 void GradientProvider::drawUI()
 {
-	//ImGui::SeparatorText("Gradient");
+    GradientEditor(_stops);
+    ImGui::ColorEdit4("color edit", &_stops[_selectedStop].color[0]);
+    
+	////ImGui::SeparatorText("Gradient");
+	//auto a = ImGui::GetWindowDrawList();
+
+	//ImVec4 color(1, 1, 1, 1);
+	//a->AddRectFilled(a->GetClipRectMin(), ImVec2(200, 200), ImGui::GetColorU32(color));
+	////a->AddRectFilledMultiColor(a->GetClipRectMin())
 }
+bool GradientProvider::GradientEditor(std::vector<GradientStop>& stops, ImVec2 size) {
+    ImDrawList* draw_list = ImGui::GetWindowDrawList();
+    ImVec2 p0 = ImGui::GetCursorScreenPos();
+    p0.x += 5;
+    size.x =ImGui::GetWindowWidth()-25;
+    ImVec2 p1 = ImVec2(p0.x + size.x, p0.y + size.y);
+
+    // Gradient bar background
+    draw_list->AddRectFilled(p0, p1, IM_COL32(0, 0, 0, 255)); // 50 50 50 255
+
+    // Draw each segment
+    for (size_t i = 0; i + 1 < _stops.size(); i++) {
+        auto a = _stops[i].color;
+        auto b = _stops[i + 1].color;
+        ImVec4 c0(a.r, a.g, a.b, a.a);
+        ImVec4 c1(b.r, b.g, b.b, b.a);
+        ImU32 col0 = ImGui::ColorConvertFloat4ToU32(c0);//(_stops[i].color);
+        ImU32 col1 = ImGui::ColorConvertFloat4ToU32(c1);//(_stops[i + 1].color);
+        float x0 = p0.x + _stops[i].position * size.x;
+        float x1 = p0.x + _stops[i + 1].position * size.x;
+        draw_list->AddRectFilledMultiColor(
+            ImVec2(x0, p0.y), ImVec2(x1, p1.y),
+            col0, col1, col1, col0
+        );
+    }
+
+    ImGui::InvisibleButton("gradientRect", size);
+
+    if (ImGui::IsItemActive() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_::ImGuiMouseButton_Left)) {
+        float newPos = (ImGui::GetIO().MousePos.x - p0.x) / size.x;
+        float clampedNewPos = std::clamp(newPos, 0.0f, 1.0f);
+
+        addStop(GradientStop{ clampedNewPos,{1,0,0,1} });
+        std::cout << "new stop added" << std::endl;
+    }
+
+
+
+
+    // Draw stop handles
+    bool changed = false;
+    for (size_t i = 0; i < _stops.size(); i++) {
+        float x = p0.x + _stops[i].position * size.x;
+        ImVec2 pos(x, p1.y);
+        ImVec2 handleMin(pos.x - 5, p1.y);
+        ImVec2 handleMax(pos.x + 5, p1.y + 10);
+
+        ImGui::SetCursorScreenPos(handleMin);
+        ImGui::InvisibleButton(("stop" + std::to_string(i)).c_str(), ImVec2(10, 10));
+
+        if (ImGui::IsItemActive() && ImGui::IsMouseClicked(ImGuiMouseButton_::ImGuiMouseButton_Left))
+            _selectedStop = i;
+        if (ImGui::IsItemActive() && ImGui::IsMouseDragging(0)) {
+            float newPos = (ImGui::GetIO().MousePos.x - p0.x) / size.x;
+            float safeOffset = 0.02;
+            if (i == 0) {
+                _stops[i].position = std::clamp(newPos, 0.0f, _stops[1].position - safeOffset);
+            }
+            else if (i + 1 == _stops.size()) {
+                _stops[i].position = std::clamp(newPos, _stops[i-1].position+ safeOffset, 1.0f);
+            }
+            else {
+                _stops[i].position = std::clamp(newPos, _stops[i - 1].position+ safeOffset, _stops[i + 1].position- safeOffset);
+
+            }
+
+
+            _selectedStop = i;
+            changed = true;
+        }
+
+        draw_list->AddTriangleFilled(
+            ImVec2(pos.x, p1.y - 3),
+            ImVec2(pos.x - 5, p1.y + 3),
+            ImVec2(pos.x + 5, p1.y + 3),
+            IM_COL32(79, 79, 79, 255)
+        );
+        draw_list->AddRectFilled(
+            ImVec2(pos.x - 5, p1.y + 3),
+            ImVec2(pos.x + 5, p1.y + 15),
+            IM_COL32(79, 79, 79, 255)
+        );
+
+        draw_list->AddRectFilled(
+            ImVec2(pos.x - 3, p1.y + 6),
+            ImVec2(pos.x + 3, p1.y + 13),
+            IM_COL32(0,0,0, 255)
+        );
+        ImVec4 stopColor(_stops[i].color.r, _stops[i].color.g, _stops[i].color.b, _stops[i].color.a);
+        draw_list->AddRectFilled(
+            ImVec2(pos.x - 3, p1.y + 6),
+            ImVec2(pos.x + 3, p1.y + 13),
+            ImGui::ColorConvertFloat4ToU32(stopColor)
+        );
+    }
+    ImGui::SetCursorScreenPos(p0); // we reset cursor to gradient editor begin position
+    size.y += 20;
+    ImGui::Dummy(size); // reserve space
+    return changed;
+}
+
 
 void ConstantColorProvider::drawUI()
 {
--- ModelViewer/ColorProvider.h
+++ ModelViewer/ColorProvider.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include "Inspectable.h"
+#include <imgui.h>
 
 class IColorProvider : public IInspectable {
 public:
@@ -10,36 +11,40 @@ public:
 };
 
 
-struct GradientProviderStop {
-	float positon; // [0,1]
-	glm::vec4 rgba;
+struct GradientStop {
+	float position; // [0,1]
+	glm::vec4 color;
 };
 // TO-DO: https://en.wikipedia.org/wiki/Centripetal_Catmull%E2%80%93Rom_spline
 class GradientProvider : public IColorProvider
 {
-	std::vector<GradientProviderStop> _stops;
+	int _selectedStop{};
+	std::vector<GradientStop> _stops{ GradientStop{0,{0,1,0,1}}, GradientStop{1,{0,1,0,0}} };
 	// Maybe we can add a LUT
+
+	bool GradientEditor(std::vector<GradientStop>& stops, ImVec2 size = ImVec2(200, 40));
+
 public:
-	void addStop(GradientProviderStop stop) {
+	void addStop(GradientStop stop) {
 		_stops.push_back(stop);
-		std::sort(_stops.begin(), _stops.end(), [](GradientProviderStop a, GradientProviderStop b) {return a.positon < b.positon; });
+		std::sort(_stops.begin(), _stops.end(), [](GradientStop a, GradientStop b) {return a.position < b.position; });
 	}
 
 	glm::vec4 evaluate(float t) const {
 		if (_stops.empty()) return glm::vec4(1, 1, 1, 1);
-		else if (_stops.size() == 1) return _stops[0].rgba;
-		else if (t <= 0) return _stops[0].rgba;
-		else if (t >= 1) return _stops.back().rgba;
+		else if (_stops.size() == 1) return _stops[0].color;
+		else if (t <= 0) return _stops[0].color;
+		else if (t >= 1) return _stops.back().color;
 
 		glm::vec4 color{};
 
 		for (int i{}; i < _stops.size() - 1; i++) {
-			float a = _stops[i].positon;
-			float b = _stops[i + 1].positon;
+			float a = _stops[i].position;
+			float b = _stops[i + 1].position;
 			if (a < t && b > t) {
 				float localT = (t - a) / (b - a);
-				glm::vec4 aColor = _stops[i].rgba;
-				glm::vec4 bColor = _stops[i + 1].rgba;
+				glm::vec4 aColor = _stops[i].color;
+				glm::vec4 bColor = _stops[i + 1].color;
 				return aColor * (1 - localT) + bColor * localT;
 			}
 		}
